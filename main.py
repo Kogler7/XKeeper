663031466a01a573ea47c73e9882a4f641bfb0af
Kogler <Kogler_S@outlook.com>
2022-08-19 16:14:49 +0800

    22w01b
    添加命令行简单交互

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import argparse
+import time
+from tqdm import trange
 
 parser = argparse.ArgumentParser()
 
 # parser.add_argument('-h', '--help', help="这是一个帮助信息")
-parser.add_argument('-n', '--name', default="Kogler", help="这是一个帮助信息")
+parser.add_argument('-u', '--user', default="admin", help="user")
+parser.add_argument('-p', '--password', default="xxx", help="password")
 
 args = parser.parse_args()
-print(args)
 
-parser.parse_args()
+tag = f"@{args.user}"
+
+while True:
+    rcv = input(f"keeper{tag}>")
+    if rcv == "exit":
+        exit()
+    elif rcv == "log":
+        print(args)
+    elif rcv == "run":
+        for i in trange(10):
+            time.sleep(1)
+    else:
+        print("Unexpected Command.")
